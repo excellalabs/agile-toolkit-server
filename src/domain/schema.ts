002d1ef45541c9daf856042af795a8f903533baf
Cameron Ivey <cameron.ivey@excella.com>
2019-03-11 19:19:54 -0400

    add votes to session (#4)
    * add votes to session
    
    * run linter with autofix; remove unecessary import

--- src/domain/schema.ts
+++ src/domain/schema.ts
@@ -1,7 +1,8 @@
 import { merge } from 'lodash';
-import { typeDefs as Session, resolvers as sessionResolvers } from "./session/session";
+import { typeDefs as Session, resolvers as sessionResolvers } from './session/session';
+import { typeDefs as Vote, resolvers as voteResolvers } from './vote/vote';
 import { gql } from 'apollo-server';
-import { makeExecutableSchema } from "graphql-tools";
+import { makeExecutableSchema } from 'graphql-tools';
 
 const Query = gql`
   type Query {
@@ -16,6 +17,6 @@ const Mutation = gql`
 `;
 
 export default makeExecutableSchema({
-    typeDefs: [ Query, Mutation, Session ],
-    resolvers: merge(sessionResolvers)
+  typeDefs: [ Query, Mutation, Vote, Session ],
+  resolvers: merge(sessionResolvers, voteResolvers),
 });
--- src/domain/vote/vote.ts
+++ src/domain/vote/vote.ts
@@ -1,17 +1,35 @@
 import { gql } from 'apollo-server';
+import { getDb } from '../../db';
+import { ObjectId } from 'mongodb';
 
-const typeDefs = gql`
+export const typeDefs = gql`
   type Vote {
-    _id: String,
     value: String
   }
 
-  type Query {
+  extend type Query {
     vote (voteId: String): Vote,
     votes: [Vote]
   }
 
-  type Mutation {
-    createVote(): Vote
+  extend type Mutation {
+    createVote(sessionId: String, value: String): Session
   }
 `;
+
+export const resolvers = {
+  Query: {
+    vote: async (root, { id }) => {
+      return getDb().collection('votes').findOne({ '_id': new ObjectId(id) });
+    },
+    votes: async () => {
+      return getDb().collection('votes').find().toArray();
+    },
+  },
+  Mutation: {
+    createVote: async (root, { sessionId, value }) => {
+      const session = await getDb().collection('sessions').updateOne({ '_id': new ObjectId(sessionId) }, { $push: { 'votes': { value: value } } });
+      return getDb().collection('sessions').findOne({ '_id': new ObjectId(sessionId) });
+    },
+  },
+};
