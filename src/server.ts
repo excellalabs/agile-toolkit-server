1f4248245c3c2747776bf5c1bd6016ec9cdfda24
Cameron Ivey <cameron.ivey@excella.com>
2019-03-11 18:24:57 -0400

    Modularize  (#3)
    * create object gql schema
    
    * modularize session schema

--- src/server.ts
+++ src/server.ts
@@ -1,58 +1,12 @@
 import { ApolloServer, gql } from 'apollo-server';
-import { getDb, dbConnect } from './db';
-import { ObjectId } from 'mongodb';
-
-/**
- * Types define shape of data returned by ApolloServer.
- *
- * They are also uses to specify "queries" (fetching data) and "mutations"
- * (changing data).
- */
-const typeDefs = gql`
-  type Session {
-    _id: String,
-    data: String
-  }
-
-  type Query {
-    session(id: String): Session,
-    sessions: [Session]
-  }
-
-  type Mutation {
-    createSession(data: String): Session
-  }
-`;
-
-/**
- * Resolvers contain the implementation details for how the ApolloServer will
- * respond to a GraphQL query.
- */
-const resolvers = {
-  Query: {
-    session: async (root, { id }) => {
-      return await getDb().collection('sessions').findOne({"_id": new ObjectId(id)});
-    },
-    sessions: async () => {
-      return await getDb().collection('sessions').find().toArray();
-    }
-  },
-  Mutation: {
-    createSession: async (root, { data }) => {
-      const result = await getDb().collection('sessions').insertOne({ data: data });
-      return await getDb().collection('sessions').findOne({"_id": new ObjectId(result.insertedId)})
-    },
-  },
-};
+import { dbConnect } from './db';
+import schema from './domain/schema';
 
 /**
  * With type definitions and resolvers, we have everything we need to setup the
  * ApolloServer.
  */
-const server = new ApolloServer({
-  resolvers,
-  typeDefs,
-});
+const server = new ApolloServer({ schema });
 
 /**
  * Now we can run the ApolloServer.
